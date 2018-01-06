29340be32f32eebe73b636bd95bec062f405bb6d
boochow <boochowp@gmail.com>
2018-01-06 16:50:49 +0900

    defined GPIO_BASE and GPIO registers addresses as relative to GPIO_BASE

--- raspberrypi/gpio_registers.h
+++ raspberrypi/gpio_registers.h
@@ -1,36 +1,38 @@
 #define IOREG(X)  (*(volatile uint32_t *) (X))
+#define GPIO(X)   IOREG((X) + GPIO_BASE)
 
 // GPIO registers
 
-#define GPFSEL0 0x20200000
-#define GPFSEL1 0x20200004
-#define GPFSEL2 0x20200008
-#define GPFSEL3 0x2020000C
-#define GPFSEL4 0x20200010
-#define GPFSEL5 0x20200014
-#define GPSET0  0x2020001C
-#define GPSET1  0x20200020
-#define GPCLR0  0x20200028
-#define GPCLR1  0x2020002C
-#define GPLEV0  0x20200034
-#define GPLEV1  0x20200038
-#define GPEDS0  0x20200040
-#define GPEDS1  0x20200044
-#define GPREN0  0x2020004C
-#define GPREN1  0x20200050
-#define GPFEN0  0x20200058
-#define GPFEN1  0x2020005C
-#define GPHEN0  0x20200064
-#define GPHEN1  0x20200068
-#define GPLEN0  0x20200070
-#define GPLEN1  0x20200074
-#define GPAREN0 0x2020007C
-#define GPAREN1 0x20200080
-#define GPAFEN0 0x20200088
-#define GPAFEN1 0x2020008C
-#define GPPUD   0x20200094
-#define GPPUDCLK0 0x20200098
-#define GPPUDCLK1 0x2020009C
+#define GPIO_BASE 0x20200000
+#define GPFSEL0 0x00
+#define GPFSEL1 0x04
+#define GPFSEL2 0x08
+#define GPFSEL3 0x0C
+#define GPFSEL4 0x10
+#define GPFSEL5 0x14
+#define GPSET0  0x1C
+#define GPSET1  0x20
+#define GPCLR0  0x28
+#define GPCLR1  0x2C
+#define GPLEV0  0x34
+#define GPLEV1  0x38
+#define GPEDS0  0x40
+#define GPEDS1  0x44
+#define GPREN0  0x4C
+#define GPREN1  0x50
+#define GPFEN0  0x58
+#define GPFEN1  0x5C
+#define GPHEN0  0x64
+#define GPHEN1  0x68
+#define GPLEN0  0x70
+#define GPLEN1  0x74
+#define GPAREN0 0x7C
+#define GPAREN1 0x80
+#define GPAFEN0 0x88
+#define GPAFEN1 0x8C
+#define GPPUD   0x94
+#define GPPUDCLK0 0x98
+#define GPPUDCLK1 0x9C
 
 // GPIO Alternate function selecter values
 #define GPF_INPUT  0U
