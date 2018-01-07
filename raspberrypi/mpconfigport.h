800d176ebb2013be7122e087bb28b415b517e050
boochow <boochowp@gmail.com>
2018-01-07 13:44:07 +0900

    enabled sys module

--- raspberrypi/mpconfigport.h
+++ raspberrypi/mpconfigport.h
@@ -40,7 +40,7 @@
 #define MICROPY_PY_CMATH            (0)
 #define MICROPY_PY_IO               (0)
 #define MICROPY_PY_STRUCT           (0)
-#define MICROPY_PY_SYS              (0)
+#define MICROPY_PY_SYS              (1)
 #define MICROPY_PY_MACHINE          (1)
 #define MICROPY_PY_UTIME_MP_HAL     (1)
 #define MICROPY_CPYTHON_COMPAT      (0)
