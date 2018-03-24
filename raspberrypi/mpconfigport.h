52beb8b5967fcff537f966c01005d9da9725bd35
boochow <boochowp@gmail.com>
2018-03-24 12:19:24 +0900

    added module gpu ; raspberry pi frame buffer supported

--- raspberrypi/mpconfigport.h
+++ raspberrypi/mpconfigport.h
@@ -71,10 +71,12 @@ typedef long mp_off_t;
 
 extern const struct _mp_obj_module_t mcu_module;
 extern const struct _mp_obj_module_t utime_module;
+extern const struct _mp_obj_module_t gpu_module;
 
 #define MICROPY_PORT_BUILTIN_MODULES                       \
     { MP_ROM_QSTR(MP_QSTR_mcu), MP_ROM_PTR(&mcu_module) }, \
     { MP_ROM_QSTR(MP_QSTR_utime), MP_ROM_PTR(&utime_module) }, \
+    { MP_ROM_QSTR(MP_QSTR_gpu), MP_ROM_PTR(&gpu_module) }, \
 
 // extra built in names to add to the global namespace
 #define MICROPY_PORT_BUILTINS \
