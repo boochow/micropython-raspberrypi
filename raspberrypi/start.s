433cd64c2b6d4dcad8e46cacf48c75a20986e945
boochow <boochowp@gmail.com>
2018-03-27 00:29:13 +0900

    fix: save and restore the value of r0 and r1

--- raspberrypi/start.s
+++ raspberrypi/start.s
@@ -3,13 +3,14 @@
 
 start:
     ldr sp, =_estack
-    
+    push {r0, r1}
     /// load C15 control register c1-c1,0,0 (c1 Control Register) into r0
     mrc p15, 0, r0, c1, c0, 0
     ldr r1, =0x00400000     /// set bit 22 (U-bit)
     orr r0, r1
     mcr p15, 0, r0, c1, c0, 0
-    
+
+	pop {r0, r1}
     bl main
 
 halt:
