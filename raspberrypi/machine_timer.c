4ef6ec924d64bb721ffb37bf816509c947606a73
boochow <boochowp@gmail.com>
2018-04-28 02:24:27 +0900

    feat: supported 4 system timers (but timer 0 and 2 are used by GPU)

--- raspberrypi/machine_timer.c
+++ raspberrypi/machine_timer.c
@@ -8,55 +8,82 @@
 #include "systimer.h"
 #include "bcm283x_it.h"
 
-// Timer class uses System timer 3
-#define SYST_NUM (3)
-
 typedef enum _timer_mode_t {
     ONE_SHOT = 0,
     PERIODIC = 1,
 } timer_mode_t;
 
 typedef struct _machine_timer_obj_t {
-    mp_obj_base_t base;
-    uint32_t id;
+    const mp_obj_base_t base;
+    const uint32_t id;
+    bool active;
+    uint32_t counter;
     uint32_t period;
     timer_mode_t mode;
     mp_obj_t callback;
 } machine_timer_obj_t;
 
-static machine_timer_obj_t timer_root;
+static machine_timer_obj_t machine_timer_obj[] = {
+    {{&machine_timer_type}, 0, true, 0, 0, PERIODIC, NULL},
+    {{&machine_timer_type}, 1, false, 0, 0, PERIODIC, NULL},
+    {{&machine_timer_type}, 2, true, 0, 0, PERIODIC, NULL},
+    {{&machine_timer_type}, 3, false, 0, 0, PERIODIC, NULL},
+};
 
-static void timer_enable(void) {
-    if ((IRQ_ENABLE1 & IRQ_SYSTIMER(3)) == 0) {
-        IRQ_ENABLE1 = IRQ_SYSTIMER(3);
-        systimer->C[3] = systimer->CLO + timer_root.period;
+static void timer_enable(const int num) {
+    if ((IRQ_ENABLE1 & IRQ_SYSTIMER(num)) == 0) {
+        IRQ_ENABLE1 = IRQ_SYSTIMER(num);
+        systimer->C[num] = systimer->CLO + machine_timer_obj[num].period;
+        machine_timer_obj[num].active = true;
     }
 }
 
-static void timer_disable(void) {
-    IRQ_DISABLE1 = IRQ_SYSTIMER(3);
+static void timer_disable(const int num) {
+    IRQ_DISABLE1 = IRQ_SYSTIMER(num);
+    machine_timer_obj[num].active = false;
 }
 
 void __attribute__((interrupt("IRQ"))) irq_timer(void) {
-    if (IRQ_PEND1 & IRQ_SYSTIMER(SYST_NUM)) {
-        if (timer_root.callback) {
-            mp_sched_schedule(timer_root.callback, &timer_root);
+    int timer_value;
+
+    if (IRQ_PEND1 & 1) {
+        timer_value = systimer->C[0];
+    } else if (IRQ_PEND1 & 2) {
+        timer_value = systimer->C[1];
+    } else if (IRQ_PEND1 & 4) {
+        timer_value = systimer->C[2];
+    } else if (IRQ_PEND1 & 8) {
+        timer_value = systimer->C[3];
+    } else {
+        return;
+    }
+    /* schedule callback func for timers which value == timer_value */
+    machine_timer_obj_t *tim = &machine_timer_obj[0];
+    for (int i = 0; i < 4; i++) {
+        if (timer_value == systimer->C[i]) {
+            if (tim[i].active) {
+                tim[i].counter++;
+                if (tim[i].callback != mp_const_none) {
+                    mp_sched_schedule(tim[i].callback, &tim[i]);
+                }
+                if (tim[i].mode == PERIODIC) {
+                    systimer->C[i] += tim[i].period;
+                } else {
+                    timer_disable(i);
+                }
+            }
+            systimer->CS |= (1 << i);
         }
-        if (timer_root.mode == PERIODIC) {
-            systimer->C[SYST_NUM] += timer_root.period;
-        } else {
-            timer_disable();
-        }
-        systimer->CS |= (1 << SYST_NUM);
     }
 }
 
 STATIC mp_obj_t machine_timer_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
     mp_arg_check_num(n_args, n_kw, 1, 1, false);
-    //    machine_timer_obj_t *tim = m_new_obj(machine_timer_obj_t);
-    machine_timer_obj_t *tim = (machine_timer_obj_t*) &timer_root;
-    tim->base.type = &machine_timer_type;
-    tim->id = mp_obj_get_int(args[0]);
+    int id = mp_obj_get_int(args[0]);
+    if (id > 3) {
+        mp_raise_ValueError("invalid timer number");
+    }
+    machine_timer_obj_t *tim = (machine_timer_obj_t*) &machine_timer_obj[id];
     return tim;
 }
 
@@ -74,7 +101,8 @@ STATIC mp_obj_t machine_timer_init_helper(machine_timer_obj_t *self, size_t n_ar
     self->period = args[0].u_int;
     self->mode = args[1].u_int;
     self->callback = args[2].u_obj;
-    timer_enable();
+    self->counter = 0;
+    timer_enable(self->id);
 
     return mp_const_none;
 }
@@ -86,16 +114,18 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_KW(machine_timer_init_obj, 0, machine_timer_init)
 
 STATIC mp_obj_t machine_timer_deinit(mp_obj_t self_in) {
     machine_timer_obj_t *self = self_in;
-    if (self->id == 0) {
-        timer_disable();
-    }
+    timer_disable(self->id);
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_timer_deinit_obj, machine_timer_deinit);
 
 STATIC void machine_timer_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
     machine_timer_obj_t *self = self_in;
-    mp_printf(print, "Timer(%u); period=%u, mode=%u", self->id, self->period, self->mode);
+    if (self->active) {
+        mp_printf(print, "Timer(%u); period=%u, mode=%u, counter=%u", self->id, self->period, self->mode, self->counter);
+    } else {
+        mp_printf(print, "Timer(%u); *inactive*", self->id);
+    }
 }
 
 STATIC const mp_rom_map_elem_t machine_timer_locals_table[] = {
