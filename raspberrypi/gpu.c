d57787a74b383d645b20bcf8ef721a1c2812d2e1
boochow <boochowp@gmail.com>
2018-04-30 09:55:41 +0900

    refactor: gpu-related functions to gpu.[ch], mailbox functions to bcm283x_mailbox.[ch]

---
+++ raspberrypi/gpu.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "bcm283x_mailbox.h"
+#include "gpu.h"
+
+void rpi_fb_init(fb_info_t *fb_info) {
+    fb_info->buf_addr = 0;
+    fb_info->buf_size = 0;
+    fb_info->rowbytes = 0;
+    while(fb_info->buf_addr == 0) {
+        mailbox_write(1, (uint32_t) (fb_info + 0x40000000));
+        mailbox_read(1);
+    }
+}
+
---
+++ raspberrypi/gpu.h
@@ -0,0 +1,22 @@
+#ifndef MICROPY_INCLUDED_RPI_GPU_H
+#define MICROPY_INCLUDED_RPI_GPU_H
+
+#include <stdint.h>
+
+typedef volatile struct                                          \
+__attribute__((aligned(16))) _fb_info_t {
+    uint32_t screen_w;   //write display width
+    uint32_t screen_h;   //write display height
+    uint32_t w;          //write framebuffer width
+    uint32_t h;          //write framebuffer height
+    uint32_t rowbytes;   //write 0 to get value
+    uint32_t bpp;        //write bits per pixel
+    uint32_t offset_x;   //write x offset of framebuffer
+    uint32_t offset_y;   //write y offset of framebuffer
+    uint32_t buf_addr;   //write 0 to get value
+    uint32_t buf_size;   //write 0 to get value
+} fb_info_t;
+
+void rpi_fb_init(fb_info_t *fb_info);
+
+#endif // MICROPY_INCLUDED_RPI_GPU_H
