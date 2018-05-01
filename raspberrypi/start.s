c0201dba70844cd0992698660f0b9b648adc6fe5
boochow <boochowp@gmail.com>
2018-05-01 11:26:53 +0900

    refactor: tabs are converted to white spaces

--- raspberrypi/start.s
+++ raspberrypi/start.s
@@ -2,27 +2,27 @@
 .section .init
 
 _start:
-	// set SVC_MODE stack
+    // set SVC_MODE stack
     ldr sp, =_estack
 
-	push {r0}
-	// set IRQ_MODE stack
-	ldr r0, =0x000000d2
+    push {r0}
+    // set IRQ_MODE stack
+    ldr r0, =0x000000d2
     msr cpsr_c, r0
-	ldr sp, =0x8000
+    ldr sp, =0x8000
 
-	// set FIQ_MODE stack
-	ldr r0, =0x000000d1
-	msr cpsr_c, r0
-	ldr sp, =0x4000
+    // set FIQ_MODE stack
+    ldr r0, =0x000000d1
+    msr cpsr_c, r0
+    ldr sp, =0x4000
 
-	// set mode to SVC_MODE
-	ldr r0, =0x000000d3
-	msr cpsr_c, r0
+    // set mode to SVC_MODE
+    ldr r0, =0x000000d3
+    msr cpsr_c, r0
 
-	// jump to main
-	pop {r0}
-	bl arm_main
+    // jump to main
+    pop {r0}
+    bl arm_main
 
 halt:
     b halt
