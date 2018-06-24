185e02cef621a5334f94abeebb3514040b526bf0
boochow <boochowp@gmail.com>
2018-06-24 11:31:50 +0900

    refactor: added hcd_globals_t definition

--- raspberrypi/usbhost.h
+++ raspberrypi/usbhost.h
@@ -1,6 +1,16 @@
 #ifndef MICROPY_INCLUDED_RPI_USBHOST_H
 #define MICROPY_INCLUDED_RPI_USBHOST_H
 
+typedef struct _hcd_globals_t {
+    void *core;
+    void *host;
+    void *power;
+    void *devices;
+    void *databuffer;
+    void *keyboards;
+    void *mice;
+} hcd_globals_t;
+
 void rpi_usb_host_init(void);
 void rpi_usb_host_process(void);
 void rpi_usb_host_deinit(void);
