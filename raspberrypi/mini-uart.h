70fde1756031d5e29233c25665550f0339b451a4
ZuluSpl0it <55359362+ZuluSpl0it@users.noreply.github.com>
2020-04-27 16:03:01 -0500

    Update mini-uart.h
    added void mp_keyboard_interrupt(void); declaration

--- raspberrypi/mini-uart.h
+++ raspberrypi/mini-uart.h
@@ -10,5 +10,6 @@ uint32_t mini_uart_rx_state(void);
 void mini_uart_set_speed(uint32_t speed);
 
 void isr_irq_mini_uart(void);
+void mp_keyboard_interrupt(void);
 
 #endif // MICROPY_INCLUDED_RPI_MINI_UART_H
