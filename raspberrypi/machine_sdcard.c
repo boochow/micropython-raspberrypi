8e7ef1609a947eff1517876c48f157678040bc04
ZuluSpl0it <55359362+ZuluSpl0it@users.noreply.github.com>
2020-04-27 21:27:12 -0500

    Update machine_sdcard.c
    updated sdcard_init-vfs function to use blockdev

--- raspberrypi/machine_sdcard.c
+++ raspberrypi/machine_sdcard.c
@@ -145,17 +145,17 @@ const mp_obj_type_t machine_sdcard_type = {
 };
 
 void sdcard_init_vfs(fs_user_mount_t *vfs, int part) {
-    vfs.base.type = &mp_fat_vfs_type;
-    vfs->flags |= FSUSER_HAVE_IOCTL;
+    vfs->base.type = &mp_fat_vfs_type;
+    vfs->blockdev.flags |= MP_BLOCKDEV_FLAG_NATIVE | MP_BLOCKDEV_FLAG_HAVE_IOCTL;
     vfs->fatfs.drv = vfs;
     vfs->fatfs.part = part;
-    vfs->readblocks[0] = (mp_obj_t)&machine_sdcard_readblocks_obj;
-    vfs->readblocks[1] = (mp_obj_t)&machine_sdcard_obj;
-//  vfs->readblocks[2] = (mp_obj_t)sdcard_read_blocks; // native version
-    vfs->writeblocks[0] = (mp_obj_t)&machine_sdcard_writeblocks_obj;
-    vfs->writeblocks[1] = (mp_obj_t)&machine_sdcard_obj;
-//  vfs->writeblocks[2] = (mp_obj_t)sdcard_write_blocks; // native version
-    vfs->u.ioctl[0] = (mp_obj_t)&machine_sdcard_ioctl_obj;
-    vfs->u.ioctl[1] = (mp_obj_t)&machine_sdcard_obj;
+    vfs->blockdev.readblocks[0] = MP_OBJ_FROM_PTR(&pyb_sdcard_readblocks_obj);
+    vfs->blockdev.readblocks[1] = MP_OBJ_FROM_PTR(&pyb_sdcard_obj);
+// vfs->blockdev.readblocks[2] = MP_OBJ_FROM_PTR(sdcard_read_blocks); // native version
+    vfs->blockdev.writeblocks[0] = MP_OBJ_FROM_PTR(&pyb_sdcard_writeblocks_obj);
+    vfs->blockdev.writeblocks[1] = MP_OBJ_FROM_PTR(&pyb_sdcard_obj);
+//  vfs->blockdev.writeblocks[2] = MP_OBJ_FROM_PTR(sdcard_write_blocks); // native version
+    vfs->blockdev.u.ioctl[0] = MP_OBJ_FROM_PTR(&pyb_sdcard_ioctl_obj);
+    vfs->blockdev.u.ioctl[1] = MP_OBJ_FROM_PTR(&pyb_sdcard_obj);
 }
 
